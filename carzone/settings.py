73547330316df09b6111ade88c5946352b0313d8
yash-2115 <mailforyash21@gmail.com>
2021-07-11 22:10:08 +0530

    added humanize

--- carzone/settings.py
+++ carzone/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'cars.apps.CarsConfig',
     'ckeditor',
+    'django.contrib.humanize',
 
     'django.contrib.admin',
     'django.contrib.auth',
