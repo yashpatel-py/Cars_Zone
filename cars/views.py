169eb7bb8931f99d5a39f3097d23d8a3553d724e
yash-2115 <mailforyash21@gmail.com>
2021-07-12 20:02:27 +0530

    some minor changes

--- cars/views.py
+++ cars/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def cars(request):
     return render(request, 'cars/cars.html')
+
+def car_detail(request, id):
+    return render(request, 'cars/car_detail.html')
--- carzone/urls.py
+++ carzone/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
