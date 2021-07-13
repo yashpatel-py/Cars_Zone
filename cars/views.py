9da7cee0eced84e119a9b4fc2cb9db2307b64b58
yash-2115 <mailforyash21@gmail.com>
2021-07-13 19:43:14 +0530

    car detail page done

--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Car
 
 # Create your views here.
 def cars(request):
     return render(request, 'cars/cars.html')
 
 def car_detail(request, id):
-    return render(request, 'cars/car_detail.html')
+    single_car = get_object_or_404(Car, pk=id)
+
+    data = {
+        'single_car':single_car
+    }
+    return render(request, 'cars/car_detail.html', data)
