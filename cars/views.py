743821e55fff0cedf92f12883110d5fd5d4bc715
yash-2115 <mailforyash21@gmail.com>
2021-07-14 12:47:25 +0530

    cars page done

--- cars/views.py
+++ cars/views.py
@@ -3,7 +3,11 @@ from .models import Car
 
 # Create your views here.
 def cars(request):
-    return render(request, 'cars/cars.html')
+    cars = Car.objects.order_by('-created_at')
+    data = {
+        'cars': cars,
+    }
+    return render(request, 'cars/cars.html', data)
 
 def car_detail(request, id):
     single_car = get_object_or_404(Car, pk=id)
