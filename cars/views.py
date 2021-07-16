3037c635712b7cac7e158fd9830b4003cc3c9aa2
yash-2115 <mailforyash21@gmail.com>
2021-07-16 21:24:57 +0530

    fatching data according to keyword

--- cars/views.py
+++ cars/views.py
@@ -23,7 +23,14 @@ def car_detail(request, id):
 
 def search(request):
     cars = Car.objects.order_by('-created_at')
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            cars = cars.filter(description__icontains=keyword) # description__icontains = name contains
+    pagimator = Paginator(cars, 2)
+    page = request.GET.get('page')
+    paged_cars = pagimator.get_page(page)
     data = {
-        'cars': cars,
+        'cars': paged_cars,
     }
     return render(request, 'cars/search.html', data)
