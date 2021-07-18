45793d6e2cde5f42e2062401651115df6c386f08
yash-2115 <mailforyash21@gmail.com>
2021-07-18 22:10:53 +0530

    Created Accounts app

--- carzone/settings.py
+++ carzone/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'ckeditor',
     'django.contrib.humanize',
     'debug_toolbar',
+    'accounts.apps.AccountsConfig',
 
     'django.contrib.admin',
     'django.contrib.auth',
