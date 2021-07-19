16efc3c3fde0a6dbfea33e2dd18e40d2faf7f5b0
yash-2115 <mailforyash21@gmail.com>
2021-07-19 22:16:12 +0530

    Added login functionality

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,17 @@ from django.contrib.auth.models import User
 
 # Create your views here.
 def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid credentials')
+            return redirect('login')
     return render(request, 'accounts/login.html')
 
 def register(request):
