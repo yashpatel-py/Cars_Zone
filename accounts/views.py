fd494f16fa04903f36b04b87fc0b544994ce6719
yash-2115 <mailforyash21@gmail.com>
2021-07-20 19:25:18 +0530

    Dashboard done

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import redirect, render
 from django.contrib import messages, auth
 from django.contrib.auth.models import User
+from contacts.models import Contact
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def login(request):
@@ -45,9 +47,14 @@ def register(request):
     else:
         return render(request, 'accounts/register.html')
 
-
+@login_required(login_url='login')
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    user_inquiry = Contact.objects.order_by('-created_at').filter(user_id=request.user.id)
+    context = {
+        'inquiry': user_inquiry
+    }
+    return render(request, 'accounts/dashboard.html', context)
+
 
 def logout(request):
     if request.method == 'POST':
