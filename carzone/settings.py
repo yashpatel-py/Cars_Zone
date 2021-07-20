c5b7763b11956de9e3845057b59c0d35affc4149
yash-2115 <mailforyash21@gmail.com>
2021-07-20 21:01:21 +0530

    debug seted true

--- carzone/settings.py
+++ carzone/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l%umi^n@i9qne!tkrqu10$qo#&zon7ozuliv0h0zvhv*zkl6tg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['safe-dawn-80118.herokuapp.com', '*']
 
